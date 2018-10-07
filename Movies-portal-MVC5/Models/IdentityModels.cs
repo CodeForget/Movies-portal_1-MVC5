f1eb93e8f260ee04a0ba70322e57b8360455553f
shivam srivastava <srivastava998@gmail.com>
2018-10-07 12:09:44 +0530

    Commit !! After Enabling Migrations Add customer table to database using add-migration InitialModel -force  !!

--- Movies-portal-MVC5/Models/IdentityModels.cs
+++ Movies-portal-MVC5/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace Movies_portal_MVC5.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
