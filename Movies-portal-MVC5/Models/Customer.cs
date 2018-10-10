4123ae88ccc92387e7563133e98a5db93f666fce
shivam srivastava <srivastava998@gmail.com>
2018-10-10 08:52:44 +0530

    Commit!! Custom Validation !!

--- Movies-portal-MVC5/Models/Customer.cs
+++ Movies-portal-MVC5/Models/Customer.cs
@@ -9,13 +9,19 @@ namespace Movies_portal_MVC5.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Display(Name="Date Of Birth")]
+        [Min18YearsIfAmember]
         public DateTime? Bithday { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
         [Display(Name= "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
---
+++ Movies-portal-MVC5/Models/Min18YearsIfAmember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movies_portal_MVC5.Models
+{
+    public class Min18YearsIfAmember:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId==0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+            if (customer.Bithday == null)
+                return new ValidationResult("Birth Date Is Required.");
+            var age = DateTime.Now.Year - customer.Bithday.Value.Year;
+
+            return (age >= 18) ? ValidationResult.Success : new ValidationResult("Customer Should be 18 Years Old");
+
+              
+        }
+    }
+}
